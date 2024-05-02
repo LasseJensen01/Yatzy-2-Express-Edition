516c0c1372ef032cc23ef08a68c35f82fb0f323c
Lasse Jensen <eaa23laje@students.eaaa.dk>
2024-05-02 10:58:00 +0200

    Made a list in login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,12 +1,14 @@
 let buttonNode = document.getElementById('register')
-let usernameInput = document.getElementsByName('username')
-let userList = document.getElementById("playerList")
+let usernameInput = document.getElementById('userName')
+let userList = document.getElementById('playerList')
 buttonNode.addEventListener('click', (event)=>{
     //TODO
     // Hvad der skal ske når vi klikker på register knap i login
     console.log("Dakker");
     let name = usernameInput.value;
-    userList.appendChild("Dakker")
+    let listItem = document.createElement('li')
+    listItem.textContent = name
+    userList.appendChild(listItem)
 })
 
 
